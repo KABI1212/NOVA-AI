f21908bd68394f8c91fd0bb58041960f5dc67e46
Kabilesh <kabileshk702@gmail.com>
2026-04-27 15:45:54 +0530

    Updated user profile page with new design

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,10 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
+import { registerServiceWorker } from "./registerServiceWorker";
 import "./styles/index.css";
+
+registerServiceWorker();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
